3e212371e86d7787bc8eb3ef7aa5a1e192f4d087
cristian <cristiantcjti@gmail.com>
2021-03-04 19:06:20 -0300

    #2 Models task,tasklist e migration da task

--- server/core/models.py
+++ server/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
 
 class User(AbstractUser):
     pass
@@ -21,5 +22,18 @@ class Task(models.Model):
     id = models.IntegerField(primary_key=True)
     tasklist = models.ForeignKey(Tasklist, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
-    description = models.TextField()
-    active = models.BooleanField()
+    description = models.TextField(max_length=255)
+    completed = models.BooleanField(null=True, blank=True)
+    watch = models.BooleanField(null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    due_time = models.TimeField(null=True, blank=True)
+    order = models.IntegerField(null=True)
+    creation_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title        
+    
+    class Meta:
+        verbose_name = "Nota"
+        verbose_name_plural = "Notas"
