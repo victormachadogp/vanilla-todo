3049a89e95abcbe5dd94fca5b97ef58601de149e
tiagolpires <tiagolpires@hotmail.com>
2021-03-06 17:35:53 -0300

    Integração GET com a API

---
+++ client/script.js
@@ -0,0 +1,30 @@
+const API_BASE = 'https://jsonplaceholder.typicode.com/todos'
+let todosContainer = document.querySelector('.todos-container')
+
+getAllTodos()
+
+async function getAllTodos () {
+    const response = await fetch(`${API_BASE}`)
+    const todos = await response.json()
+
+    todos.map((todo) => {
+        let isCompleted = false
+        let isHighlighted = false
+        if(todo.completed){isCompleted = 'completed'}
+        if(todo.highlight){isHighlighted = 'highlighted'}
+
+        const todoContent = `
+            <div id="${todo.id}" class="todo-card ${isCompleted} ${isHighlighted}">
+                <div class="todo-title-status-container">
+                    <img src="./icons/checkbox.svg" class="check-off" />
+                    <img src="./icons/checkbox-on.svg" class="check-on" />
+                    <span>${todo.title}</span>   
+                </div>
+                <img src="./icons/star.svg" class="highlight-off" />
+                <img src="./icons/star-on.svg" class="highlight-on" />
+            </div>
+        `
+        
+        todosContainer.innerHTML += todoContent
+    })
+}
