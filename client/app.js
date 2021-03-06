214f71f0b5a110d7a3c99cb45e2712bed8a1c7eb
tiagolpires <tiagolpires@hotmail.com>
2021-03-06 20:57:27 -0300

    Adicionado operador ternário

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+// Navbar
 let btnNav = document.querySelector(".btn-nav");
 let navContainer = document.querySelector(".nav-menu");
 let btnClose = document.querySelector(".btn-close");
@@ -11,4 +12,31 @@ function openNavMenu() {
 
 function closeNavMenu() {
   navContainer.style.display = "none";
+}
+
+// Fetch API
+const apiBaseUrl = 'https://jsonplaceholder.typicode.com/todos'
+let todosContainer = document.querySelector('.todos-container')
+
+getAllTodos()
+
+async function getAllTodos () {
+  const response = await fetch(`${apiBaseUrl}`)
+  const todos = await response.json()
+
+  todos.map((todo) => {
+    const todoContent = `
+      <div id="${todo.id}" class="todo-card ${todo.completed ? "completed" : ""} ${todo.highlighted ? "highlighted" : ""}">
+        <div class="todo-title-status-container">
+          <img src="./icons/checkbox.svg" class="check-off" />
+          <img src="./icons/checkbox-on.svg" class="check-on" />
+          <span>${todo.title}</span>   
+        </div>
+        <img src="./icons/star.svg" class="highlight-off" />
+        <img src="./icons/star-on.svg" class="highlight-on" />
+      </div>
+    `
+
+    todosContainer.innerHTML += todoContent
+  })
 }
