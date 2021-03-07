09b0b6e3c154d2e931d88b819bcb81f14250fa78
Daniel Fonseca <dfcode@gmail.com>
2021-03-07 15:36:02 -0300

    #22 Criacao da rota teste tasklists/

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tasklists/', views.get_tasklists),
 ]
