aefda1e331408234c485571765f845a744b4c11f
Emerson Delatorre <emerson@delatorre.dev>
2021-03-14 21:28:35 -0300

    #30 Criado metodo POST para inserir nova Tasklist

--- server/core/views.py
+++ server/core/views.py
@@ -1,24 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse 
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
+from django.core import serializers
 from .models import Tasklist
+import sys, json
 
 
-# Create your views here.
-
+@csrf_exempt
 @require_http_methods(["GET","POST"]) 
-def tasklists(request):
-    try: 
-        tasks = Tasklist.objects.all().values()  
-    except Tasklists.DoesNotExist: 
-        return JsonResponse({'message': 'Tasklists do not exist'}, status=status.HTTP_404_NOT_FOUND) 
-    
+def tasklists(request):    
     if request.method == "GET":
+        try:
+            tasks = Tasklist.objects.all().values()  
+        except Tasklist.DoesNotExist: 
+            return JsonResponse(
+                {'message': 'Tasklists do not exist'}, 
+                status = 404
+            ) 
+
         task_list = list(tasks)  
         return JsonResponse(task_list, safe=False)
+
     else:
-        return HttpResponse("POST")    
+        data = json.loads(request.body)
+
+        tsk = Tasklist()
+        if "title" in data: 
+            tsk.title = data['title']
+        if "color" in data:  
+            tsk.color = data['color']
+        if "order" in data:
+            tsk.order = data['order']  
+
+        try:
+            tsk.save()
+        except:
+            return JsonResponse(
+                {'message': sys.exc_info()[0]},
+                 status=500)
+       
+        return JsonResponse(
+            {'message': 'Created'},
+            status=201
+        )
 
 
 @require_http_methods(["PUT","DELETE","GET"])
@@ -38,7 +64,7 @@ def tasklistsid(request, tasklist_id):
 # Tasklists
 # GET /tasklists/ - retorna todas as tasklists -  ✅
 # GET /tasklists/:id/ - retorna apenas uma tasklist ✅ 
-# POST /tasklists/ - cria uma nova tasklist 
+# POST /tasklists/ - cria uma nova tasklist ✅
 # PUT/PATCH? /tasklists/:id/ - atualiza apenas uma tasklist
 # DELETE /tasklists/:id/ - deleta apenas uma tasklist
 
