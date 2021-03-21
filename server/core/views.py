393cafcebf0547b09a034f7a2372baccc72c4f38
cristian <cristiantcjti@gmail.com>
2021-03-21 19:10:48 -0300

    #33 Realizado GET para retorno das tasks de uma tasklist.

--- server/core/views.py
+++ server/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse 
 from django.views.decorators.http import require_http_methods
 from django.forms.models import model_to_dict
-from .models import Tasklist
+from .models import Tasklist, Task
 
 
 # Create your views here.
@@ -24,7 +24,8 @@ def tasklists(request):
 @require_http_methods(["PUT","DELETE","GET"])
 def tasklistsid(request, tasklist_id):
     try: 
-        tasklist = Tasklist.objects.get(id=tasklist_id)         
+        tasklist = Tasklist.objects.get(id=tasklist_id)    
+        print("Testing....:", tasklist)     
     except Tasklist.DoesNotExist: 
         return JsonResponse({'message': 'Tasklist_id does not exist'}, status=status.HTTP_404_NOT_FOUND) 
 
@@ -35,6 +36,17 @@ def tasklistsid(request, tasklist_id):
     else: 
         return HttpResponse("DELETE") 
 
+#TASKS
+@require_http_methods(["GET"]) 
+def tasklists_id_tasks(request, tasklist_id):
+    try:
+        tasks = Task.objects.get(id=tasklist_id)
+    except Task.DoesNotExist:
+        return JsonResponse({'message': 'There is no task with the requested id'})
+    
+    if request.method == "GET":
+        return JsonResponse(model_to_dict(tasks), safe=False)
+
 # Tasklists
 # GET /tasklists/ - retorna todas as tasklists -  ✅
 # GET /tasklists/:id/ - retorna apenas uma tasklist ✅ 
@@ -43,7 +55,7 @@ def tasklistsid(request, tasklist_id):
 # DELETE /tasklists/:id/ - deleta apenas uma tasklist
 
 # Tasks
-# GET /tasklists/:id/tasks/ - retorna todas as tasks de uma determinada tasklist
+# GET /tasklists/:id/tasks/ - retorna todas as tasks de uma determinada tasklist ✅
 # POST /tasklists/:id/tasks/ - cria uma nova task
 # PUT/PATCH? /tasks/:id/ - atualiza apenas uma task de uma determinada tasklist
 # DELETE /tasks/:id/ - deleta apenas uma task
