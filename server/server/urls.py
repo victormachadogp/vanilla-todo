7296151050b59d5b11cd7577a80ef19d3ee7d4c9
LivSith <liviasithmendes@gmail.com>
2021-05-09 13:26:46 -0300

    #32 - Criado metodo DELETE da tasklist.

--- server/server/urls.py
+++ server/server/urls.py
@@ -22,7 +22,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasklists/', views.tasklists),
-    path('tasklists/<int:tasklist_id>/', views.tasklistsid),
+    path('tasklists/<int:tasklist_id>', views.tasklistsid),
     path('tasklists/<int:tasklist_id>/tasks', views.tasklists_id_tasks),
     path('task/<int:task_id>', views.task_id)
 ]
