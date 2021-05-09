327065187dbef8a31f48b478f786cb3570f16136
rafaelreis <rafyangtj@gmail.com>
2021-05-09 19:28:11 -0300

    Retirado as barras '/' no final das urls - motivo padronização

--- server/urls.py
+++ server/urls.py
@@ -20,9 +20,9 @@ from core import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tasklists/', views.tasklists),
-    path('tasklists/<int:tasklist_id>/', views.tasklistsid),
+    path('admin', admin.site.urls),
+    path('tasklists', views.tasklists),
+    path('tasklists/<int:tasklist_id>', views.tasklistsid),
     path('tasklists/<int:tasklist_id>/tasks', views.tasklists_id_tasks),
     path('task/<int:task_id>', views.task_id)
 ]
